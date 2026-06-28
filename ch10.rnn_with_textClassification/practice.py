c59bcaa01699507d4e13cc0f959b18931c8c367d
chojo5195 <chojo5195@gmail.com>
2026-06-28 23:55:07 +0900

    영화 리뷰 감정

--- ch10.rnn_with_textClassification/practice.py
+++ ch10.rnn_with_textClassification/practice.py
@@ -69,7 +69,6 @@ tokenizer = Tokenizer()
 tokenizer.fit_on_texts(X_train)
 X_train_encoded = tokenizer.texts_to_sequences(X_train)
 print(X_train_encoded[:5])
-
 # %%
 # 단어 유추
 word_to_index = tokenizer.word_index
@@ -138,3 +137,291 @@ history = model.fit(X_train_padded, y_train, epochs=4, batch_size=64, validation
 X_test_encoded = tokenizer.texts_to_sequences(X_test)
 X_test_padded = pad_sequences(X_test_encoded, maxlen = max_len)
 print("\n 테스트 정확도: %.4f" % (model.evaluate(X_test_padded, y_test)[1]))
+
+# %%
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Embedding
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+vocab_size = 1000
+max_len = 100
+
+(X_train, y_train), (X_test, y_test) = reuters.load_data(num_words=vocab_size, test_split=0.2)
+
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+embedding_dim = 128
+hidden_units = 128
+num_classes = 46
+
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim))
+model.add(LSTM(hidden_units))
+model.add(Dense(num_classes, activation='softmax'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+history = model.fit(X_train, y_train, batch_size=128, epochs=30, callbacks=[es, mc], validation_data=(X_test, y_test))
+
+# %%
+# 리뷰 감성 분류
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import imdb
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data()
+print('훈련용 리뷰 개수 : {}'.format(len(X_train)))
+print('테스트용 리뷰 개수 : {}'.format(len(X_test)))
+num_classes = len(set(y_train))
+print('카테고리 : {}'.format(num_classes))
+
+# %%
+reviews_length = [len(review) for review in X_train]
+print('리뷰의 최대 길이 : {}'.format(np.max(reviews_length)))
+print('리뷰의 평균 길이 : {}'.format(np.mean(reviews_length)))
+
+plt.subplot(1,2,1)
+plt.boxplot(reviews_length)
+plt.subplot(1,2,2)
+plt.hist(reviews_length, bins=50)
+plt.show()
+
+# %%
+unique_elements, counts_elements = np.unique(y_train, return_counts=True)
+print("각 레이블에 대한 빈도수:")
+print(np.asarray((unique_elements, counts_elements)))
+
+# %%
+# .get_word_index(): 단어와 맵핑도는 정수
+# imdb.get__word_index()+3: 실제 맵핑되는 정수(IMDB 정한 규칙)
+
+word_to_index = imdb.get_word_index()
+index_to_word={}
+for key,value in word_to_index.items():
+    index_to_word[value+3] = key
+
+print('빈도수 상위 1등 단어 : {}'.format(index_to_word[4]))
+
+# %%
+for index, token in enumerate(("<pad>", "<sos>", "<unk>")):
+  index_to_word[index] = token
+
+# %%
+# 단어 크기: < 10,000
+# 리뷰 길이: < 500
+ 
+import re
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, GRU, Embedding
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+vocab_size = 10000
+max_length = 500
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=vocab_size)
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+embedding_dim = 100
+hidden_units = 128
+
+model = Sequential()
+model.add(Embedding(vocab_size,embedding_dim))
+model.add(GRU(hidden_units))
+model.add(Dense(1,activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('GRU_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=64, validation_split=0.2)
+
+
+# %%
+# 네이버 영화 리뷰
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request
+from konlpy.tag import Okt
+from tqdm import tqdm
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt", filename="ratings_train.txt")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt", filename="ratings_test.txt")
+
+train_data = pd.read_table('ratings_train.txt')
+test_data = pd.read_table('ratings_test.txt')
+
+# %%
+print('훈련용 리뷰 개수 :',len(train_data)) 
+
+# 데이터 정제
+# 중복값
+train_data['document'].unique(), train_data['label'].unique()
+train_data.drop_duplicates(subset=['document'], inplace=True)
+
+print('총 샘플의 수 :',len(train_data))
+# %%
+train_data['label'].value_counts().plot(kind='bar')
+print(train_data.isnull().values.any())
+# %%
+print(train_data.isnull().sum())
+
+# %%
+# null 제거
+train_data = train_data.dropna(how='any')
+print(train_data.isnull().sum())
+
+# %%
+# 한글,공백 제외 모두 제거 document 
+train_data['document'] = train_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True)
+# %%
+# 한글이 아닌 영어,숫자로만 구성된 평가일 경우 체크
+train_data['document'] = train_data['document'].str.replace('^ +', "", regex=True) # white space 데이터를 empty value로 변경
+train_data['document'] = train_data['document'].replace('',np.nan)
+print(train_data['document'].isnull().sum())
+# %%
+train_data = train_data.dropna(how='any')
+# %%
+# %%
+# test_data 동일 진행
+test_data.drop_duplicates(subset = ['document'], inplace=True)
+test_data['document'] = test_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True)
+test_data['document'] = test_data['document'].str.replace('^ +', "", regex=True) 
+test_data['document'] = test_data['document'].replace('',np.nan)
+test_data = test_data.dropna(how='any')
+# %%
+okt = Okt()
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+X_train = []
+for sentence in tqdm(train_data['document']):
+    tokenized_sentence = okt.morphs(sentence,stem=True) # 토큰화
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords]
+    X_train.append(stopwords_removed_sentence)
+# %%
+X_test = []
+for sentence in tqdm(test_data['document']):
+    tokenized_sentence = okt.morphs(sentence, stem=True) # 토큰화
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords] # 불용어 제거
+    X_test.append(stopwords_removed_sentence)
+
+# %%
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+# print(tokenizer.word_index) word_index (어떤 단어 : index )
+# %%
+threshold = 3
+total_cnt = len(tokenizer.word_index)
+rare_cnt = 0
+total_freq = 0
+rare_freq = 0
+
+for key,value in tokenizer.word_counts.items():
+    total_freq += value
+
+    if (value<threshold):
+        rare_cnt+=1
+        rare_freq+=value
+
+print('단어 집합(vocabulary)의 크기 :',total_cnt)
+print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
+# %%
+vocab_size = total_cnt - rare_cnt + 1
+print('단어 집합의 크기 :',vocab_size)
+# %%
+tokenizer = Tokenizer(vocab_size)
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+y_train = np.array(train_data['label'])
+y_test = np.array(test_data['label'])
+
+# %%
+# 길이가 0인 샘플 제거
+filtered = [(x, y) for x, y in zip(X_train, y_train) if len(x) > 1]
+X_train, y_train = zip(*filtered)
+X_train = list(X_train)            # 패딩 전이라 리스트 OK
+y_train = np.array(y_train)
+
+# %%
+print('리뷰의 최대 길이 :',max(len(review) for review in X_train))
+print('리뷰의 평균 길이 :',sum(map(len, X_train))/len(X_train))
+plt.hist([len(review) for review in X_train], bins=50)
+plt.xlabel('length of samples')
+plt.ylabel('number of samples')
+plt.show()
+
+# %%
+def below_threshold_len(max_len,nested_list):
+    count = 0
+    for sentence in nested_list:
+        if(len(sentence) <= max_len):
+            count +=1
+    print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (count / len(nested_list))*100))
+# %%
+max_len = 30
+below_threshold_len(max_len,X_train)
+# %%
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+# %%
+# 검증
+from tensorflow.keras.layers import Embedding, Dense, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+embedding_dim = 100
+hidden_units = 128
+
+model = Sequential()
+model.add(Embedding(vocab_size,embedding_dim))
+model.add(LSTM(hidden_units))
+model.add(Dense(1,activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+X_train = np.array(X_train)   # pad_sequences 결과라 이미 배열이지만 안전하게
+X_test  = np.array(X_test)
+y_train = np.array(y_train)   # ← 핵심: 리스트 → NumPy 배열
+y_test  = np.array(y_test)
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=64, validation_split=0.2)
+
+# %%
+loaded_model = load_model('best_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+
+# %%
+def sentiment_predict(new_sentence):
+  new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]','', new_sentence)
+  new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
+  new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
+  encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+  pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
+  score = float(loaded_model.predict(pad_new)) # 예측
+  if(score > 0.5):
+    print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(score * 100))
+  else:
+    print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - score) * 100))
